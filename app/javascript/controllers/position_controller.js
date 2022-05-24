40b8f3d5fb619e9e27f7b4a7ad0532cd46e3a330
hayataslam401 <hayataslam401@gmail.com>
2022-05-24 14:33:04 +0500

    add position controller

---
+++ app/javascript/controllers/position_controller.js
@@ -0,0 +1,27 @@
+import { Controller } from "@hotwired/stimulus"
+import Sortable from "sortablejs"
+import Rails from "@rails/ujs"
+export default class extends Controller {
+  connect() {
+    this.sortable = Sortable.create(this.element,{
+      group: 'shared',
+      animation: 150,
+      onEnd: this.end.bind(this)
+    })
+  }
+
+  end(event) {
+    let id = event.item.id
+    let data = new FormData()    
+    data.append("position", event.newIndex + 1)
+    console.log(event.newIndex + 1 )
+    var url = "/movies/:id/move".replace(":id", id)
+    Rails.ajax({
+      url: url,
+      type: 'PATCH',
+      data: data
+      
+    })
+    
+  }
+} 
